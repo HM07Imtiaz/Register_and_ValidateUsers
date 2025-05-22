a0bfbe856718bf6aff3e4935925cf5033648de13
HM07 <hmimtiaz2@gmial.com>
2025-05-23 02:20:47 +0600

    Final Version

--- db.js
+++
@@ -1,7 +0,0 @@
-// db.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://eszpqxqohpxkkmlnlbdd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzenBxeHFvaHB4a2ttbG5sYmRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MzcwMjMsImV4cCI6MjA2MzUxMzAyM30.JyoWXYwck9-t77_oAnWRqgNXSlcRFqsfpiCBo4scCXI';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
